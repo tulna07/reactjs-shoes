5f57b1bfd3da49f553870970c8821acb83b56322
scoobytux <tu.lna07@gmail.com>
2022-05-29 13:09:27 +0700

    Add minor change

--- src/shoes-store/product-item.js
+++ src/shoes-store/product-item.js
@@ -6,7 +6,11 @@ export default class ProductItem extends Component {
 
     return (
       <div className="card mb-4">
-        <img className="card-img-top" src={product.image} alt="img" />
+        <img
+          className="card-img-top"
+          src={product.image}
+          alt={`image ${product.id}`}
+        />
         <div className="card-body">
           <h6 className="card-title">{product.name}</h6>
           <p className="card-price">{`${product.price} $`}</p>
