c1d4ac85d67872cf984c7e14b1afffe47db6db3c
scoobytux <tu.lna07@gmail.com>
2022-05-29 22:22:36 +0700

    Add footer component

---
+++ src/shoes-store/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="text-center py-3">
+        Inspired by <a href="https://www.nike.com/vn/">Nike</a>
+      </footer>
+    );
+  }
+}
